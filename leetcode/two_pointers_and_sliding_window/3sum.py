e0b51bc0af68ef459d23311d7099601c58f73e2a
Jesus Kevin Walker <jesus.walker3000@gmail.com>
2025-05-30 19:05:36 -0700

    Update 3sum.py
    updated implementation & added a test case

--- leetcode/two_pointers_and_sliding_window/3sum.py
+++ leetcode/two_pointers_and_sliding_window/3sum.py
@@ -1,33 +1,31 @@
 from typing import List
 
 class Solution:
-    def two_sum(self,seen_nums,  nums, target, index):
-        for i in range(len(nums)):
-            if i == index:
-                continue
-            if target - nums[i] in seen_nums:
-                return [i, seen_nums[target - nums[i]]]
-        return []
+    def two_sum(self, nums, target, target_index):
+        # print('**')
+        seen_nums = {}
+        results = []
+        seen = set()
+        for i in range(target_index, len(nums)):
+            complement = -target - nums[i]
+            if complement in seen_nums and f"{target}{complement}{nums[i]}" not in seen:
+                seen.add(f"{target}{complement}{nums[i]}")
+                results.append([target, complement, nums[i]])
+            seen_nums[nums[i]] = i
+        return results
 
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         results = []
-        seen_nums = {num: index for index, num in enumerate(nums)}
-        seen_indexes = {}
+        nums.sort()
         for i in range(len(nums)-1):
-            vals = self.two_sum(seen_nums, nums, nums[i] * -1, index)
-            if not vals:
+            if i>0 and nums[i] == nums[i-1]:
                 continue
-            vals.extend([i])
-            vals.sort()
-            new_vals = [str(i) for i in vals]
-            key = "".join(new_vals)
-            if key in seen_indexes:
-                continue
-            seen_indexes[key] = [nums[value] for value in vals]
-        print(seen_indexes)
-        return [values for _, values in seen_indexes.items()]
+            results += self.two_sum(nums, nums[i], i + 1)
+        return results
+
 
 if __name__ == "__main__":
     print(Solution().threeSum([-1,0,1,2,-1,-4])) # [[-1,-1,2],[-1,0,1]]
     print(Solution().threeSum([0,1,1])) # []
     print(Solution().threeSum([0,0,0]))   # [[0,0,0]]
+    print(Solution().threeSum([0,0,0,0]))   # [[0,0,0]]
